d091b84cd51ed108cc3b82b3a5733738e35e465a
jamieberrier <berrier.jamie@gmail.com>
2019-09-12 19:57:44 +0000

    create user login and sign up routes and login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,57 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index"
-  end
-
   # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new"
+  get "/signup" do
+    if !logged_in?
+      erb :"/users/new"
+    else
+      redirect "/users/#{@user.id}"
+    end
   end
 
   # POST: /users
-  post "/users" do
-    redirect "/users"
+  post "/signup" do
+    @user = User.create(params[:user])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  end
+
+  get "/login" do
+    if !logged_in?
+      erb :"/users/login"
+    else
+      redirect "/users/#{current_user.id}"
+    end
+  end
+
+  post "/login" do
+    login
+  end
+
+  get "/logout" do
+    logout!
   end
 
   # GET: /users/5
+  # use slug instead of id
   get "/users/:id" do
     erb :"/users/show"
   end
 
   # GET: /users/5/edit
+  # If user wants to edit their profile
   get "/users/:id/edit" do
     erb :"/users/edit"
   end
 
   # PATCH: /users/5
+  # If user wants to edit their profile
   patch "/users/:id" do
     redirect "/users/:id"
   end
 
   # DELETE: /users/5/delete
+  # If user wants to delete their profile
   delete "/users/:id/delete" do
     redirect "/users"
   end
