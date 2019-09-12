23ac9bd85e051983dc3da67907e199f345782016
jamieberrier <berrier.jamie@gmail.com>
2019-09-12 19:40:05 +0000

    add helper methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,34 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def login
+      user = User.find_by(username: params[:email])
+
+      if user && user.authenticate(params[:password])
+	       session[:user_id] = user.id
+         redirect "/users/#{user.id}"
+      else
+        redirect "/login"
+      end
+    end
+
+    def current_user
+      # if @current_user is assigned, don't evaluate
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def authorized_to_edit?(recipe)
+      current_user == recipe.user
+    end
+
+    def logout!
+      session.clear
+      redirect "/login"
+    end
+  end
 end
